21d9e42ec32cb58f83161c9e82899417cdad1aff
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-09-21 10:14:54 +0200

    Implemented create new author(POST) and List all authors(GET) methods

---
+++ src/controllers/bookController.ts
@@ -0,0 +1,41 @@
+import type { Request, Response,} from "express";
+import { books, type Book,} from "../models/Book.js";
+import { authors } from "../models/Author.js";
+
+// GET books
+export const getBooks = (
+  req: Request,
+  res: Response,
+) => {
+  res.status(200).json(books);
+};
+
+// POST books
+export const createBook = (
+  req: Request,
+  res: Response,
+) => {
+  const { title, year, authorId } = req.body;
+
+  // Check if the author exists
+  const author = authors.find(
+    (author) => author.id === authorId,
+  );
+
+  if (!author) {
+    return res.status(400).json({
+      message: "Invalid authorId",
+    });
+  }
+
+  const newBook: Book = {
+    id: books.length + 1,
+    title,
+    year,
+    authorId,
+  };
+
+  books.push(newBook);
+
+  res.status(201).json(newBook);
+};
