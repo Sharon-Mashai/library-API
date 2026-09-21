a3e0410ea08407dc69dbbad271527f3eaacce94c
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-09-21 09:49:33 +0200

    Added all the CRUD routes in the author routes file

--- src/routes/authorRoutes.ts
+++ src/routes/authorRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { createAuthor, getAuthors,} from "../controllers/authorController.js";
+import { createAuthor, deleteAuthor, getAuthorById, getAuthors, updateAuthor,} from "../controllers/authorController.js";
 
 const router = Router();
 
 router.get("/", getAuthors);
 
+router.get("/:id", getAuthorById);
+
 router.post("/", createAuthor);
+
+router.put("/:id", updateAuthor);
+
+router.delete("/:id", deleteAuthor);
 
 export default router;
