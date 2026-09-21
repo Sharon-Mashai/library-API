b33001628c286a85094bb4c02faedd6313fd87ba
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-09-21 12:09:11 +0200

    Connected the error handler file to the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authorRoutes from "./routes/authorRoutes.js";
 import bookroutes from "./routes/bookRoutes.js" ;
 import { logger } from "./middleware/logger.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 const PORT = 4000;
@@ -10,9 +11,19 @@ const PORT = 4000;
 app.use(express.json());
 app.use(logger);
 
-// Author routes
+// routes
 app.use("/authors", authorRoutes);
 app.use("/books", bookroutes);
+
+// Handle unknown routes
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Route not found",
+  });
+});
+
+// Centralized error handler
+app.use(errorHandler);
 
 // Start server
 app.listen(PORT, () => {
